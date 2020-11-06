6d2048d67e937d040bce4baeddfad9e2e6710306
Pavel Sobolev <paveloom@mail.ru>
2020-11-06 12:09:43 +0300

    Add the `Docs & Code Coverage` workflow.

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#HillRegions.jl","page":"Home","title":"HillRegions.jl","text":"","category":"section"}]
+}
---
+++ docs/src/index.md
@@ -0,0 +1,1 @@
+# HillRegions.jl
