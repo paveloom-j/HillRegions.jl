330ee3b0f27e0c7e47b4675222ed71247ab9e1ee
Pavel Sobolev <paveloom@mail.ru>
2020-11-07 17:13:12 +0300

    Remove the `HillRegion(C::C_TYPE; μ::M_TYPE)` constructor.

--- src/include/types/HillRegion.jl
+++ src/include/types/HillRegion.jl
@@ -20,4 +20,3 @@ Base.@kwdef struct HillRegion
 end
 
 HillRegion(μ::M_TYPE; C::C_TYPE) = HillRegion(μ, C)
-HillRegion(C::C_TYPE; μ::M_TYPE) = HillRegion(μ, C)
--- test/tests/TestHillRegion.jl
+++ test/tests/TestHillRegion.jl
@@ -9,7 +9,12 @@ using Test
     C = 3.652916
 
     @test_nowarn HillRegion(μ = μ, C = C)
-    @test_nowarn HillRegion(C, μ = μ)
+    @test_nowarn HillRegion(μ, C = C)
+    @test_nowarn HillRegion(μ, C)
+
+    μ = 1 / 11
+
+    @test_nowarn HillRegion(μ = μ, C = C)
     @test_nowarn HillRegion(μ, C = C)
     @test_nowarn HillRegion(μ, C)
 
